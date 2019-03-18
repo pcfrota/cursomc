bc3ebd931e5c4ec6386356cef07e06f838d27c93
Paulo Cesar de Almeida Frota <pcfrota@gmail.com>
2019-03-18 12:40:27 -0400

    Paginação com parametros opcionais na requisição

--- src/main/java/br/com/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/cursomc/services/CategoriaService.java
@@ -5,9 +5,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.cursomc.domain.Categoria;
+import br.com.cursomc.dto.CategoriaDTO;
 //import br.com.cursomc.dto.CategoriaDTO;
 import br.com.cursomc.repositories.CategoriaRepository;
 import br.com.cursomc.services.exceptions.DataIntegrityException;
@@ -51,14 +55,14 @@ public class CategoriaService {
 		return repo.findAll();
 	}
 	
-	/*public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
-	}*/
+	}
 	
 	private void updateData(Categoria newObj, Categoria obj) {
 		newObj.setNome(obj.getNome());
